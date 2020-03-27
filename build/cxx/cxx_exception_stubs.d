46d53c9566cadc6b691b31eb1ce51b01235da962
SachinDarade <daradesachin0@gmail.com>
2020-03-28 03:02:58 +1300

    third commit

---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/sachin/esp/esp-mdf/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ /home/sachin/esp/internal_communication/build/include/sdkconfig.h
+
+/home/sachin/esp/internal_communication/build/include/sdkconfig.h:
+/home/sachin/esp/esp-mdf/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
